6e6d7fba6eac12dcca6929703a6001b69a1655ca
ramazanyigitt <rmznygt920@gmail.com>
2023-12-17 21:57:57 +0300

    Git action test

--- lib/requests/rest_api_request.dart
+++ lib/requests/rest_api_request.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 part of '../rest_api_package.dart';
 
 enum RequestMethod {
--- lib/rest_api_package.dart
+++ lib/rest_api_package.dart
@@ -6,7 +6,7 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:flutter/foundation.dart';
-import 'package:http_parser/src/media_type.dart';
+import 'package:http_parser/http_parser.dart';
 
 part 'models/base_model.dart';
 part 'requests/file_rest_api_request.dart';
